388e0c2a721278da5f72b6c7f72b8b984667d622
Todd Hogarth <begin-again@users.noreply.github.com>
2021-03-16 15:28:41 -0400

    tinkering

---
+++ jobs.js
@@ -0,0 +1,212 @@
+/* eslint-disable no-magic-numbers */
+
+const EventEmitter = require('events');
+const uuid = require('uuid/v4');
+
+/**
+ * @class Reporter
+ * @private
+ */
+class Reporter extends EventEmitter {
+
+    addListener(...args) {
+        super.addListener(...args);
+    }
+    on(...args) {
+        super.on(...args);
+    }
+
+    fireEvent(msg, id) {
+        this.emit(msg, id);
+    }
+}
+
+
+/**
+ * @class Job
+ * @description Wrapper for task which returns a Promise
+ */
+class Job {
+
+    /**
+     *
+     * @param {Promise} payload
+     * @param {Object} [params]
+     * @param {String} [name] - job name
+     */
+    constructor(payload, params = [], name) {
+        this.payload = payload;
+        this._name = name;
+        this._result = null;
+        this.params = params;
+        this.reporter = new Reporter();
+        this.id = uuid();
+        this.isRunning = this.isDone = false;
+        this.isWaiting = true;
+        this.initNow = Date.now();
+    }
+
+    get status() {
+        if(this.isDone) {
+            return 'done';
+        }
+        if(this.isRunning) {
+            return 'running';
+        }
+        if(this.isWaiting) {
+            return 'waiting';
+        }
+        return 'unknown';
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    /**
+     * @param {String} s
+     *
+     * @memberof Job
+     */
+    set name(s) {
+        this._name = s;
+    }
+
+    /**
+     *
+     * @returns {Promise<any>}
+     */
+    exec() {
+        this.reporter.fireEvent('started', this.id);
+        this.execNow = Date.now();
+        this.isWaiting = false;
+        this.isRunning = true;
+        try {
+            return this.payload(this.params)
+                .catch(err => {
+                    return { error: err };
+                })
+                .then((result) => {
+                    this.isRunning = false;
+                    this.isDone = true;
+                    this.result = result;
+                    this.execSeconds = (Date.now() - this.execNow) / 1000;
+                    this.queueSeconds = (Date.now() - this.initNow) / 1000;
+                    this.reporter.fireEvent('done', this.id);
+                    return Promise.resolve(this.result);
+                });
+        }
+        catch (err) {
+            this.isRunning = false;
+            this.isDone = true;
+            this.error = err.message;
+            this.result = { error: this.error };
+            this.execSeconds = (Date.now() - this.execNow) / 1000;
+            this.queueSeconds = (Date.now() - this.initNow) / 1000;
+            this.reporter.fireEvent('done', this.id);
+            return Promise.resolve(this.result);
+        }
+
+    }
+
+    get result() {
+        return this._result;
+    }
+
+    set result(value) {
+        this._result = value;
+    }
+
+}
+
+
+/**
+ * @class JobQueue
+ * @description Executes Jobs in parallel while limiting concurrency
+ */
+class JobQueue {
+
+    /**
+     *
+     * @param {Array<Job>} jobs
+     * @param {Number} concurrentJobs
+     */
+    constructor(jobs = [], concurrentJobs = 1) {
+        jobs.forEach((j, i) => {
+            const good = j instanceof Job;
+            if(!good) {
+                throw new TypeError(`job at index ${i} must be instance of Job`);
+            }
+        });
+        this.concurrent = concurrentJobs;
+        this.total = jobs.length;
+        this.toDo = jobs;
+        this.running = {};
+        this.complete = [];
+        this.reporter = new Reporter();
+        this.now = Date.now();
+    }
+
+    get runAnother() {
+        return Boolean((Object.keys(this.running).length < this.concurrent) && this.toDo.length);
+    }
+
+    get done() {
+        return Boolean((Object.keys(this.running).length === 0) && (this.toDo.length === 0));
+    }
+
+    /**
+     *
+     * @param {String} msg
+     * @param {Job} job
+     * @emits Reporter: any
+     */
+    report(msg, job) {
+        const running = Object.keys(this.running).length;
+        const toDo = this.toDo.length;
+        const complete = this.complete.length;
+        const seconds = (Date.now() - this.now) / 1000;
+        const details = { running, toDo, complete, seconds };
+
+        if(job) {
+            details.id = job.id;
+        }
+        console.log(msg);
+        this.reporter.fireEvent(msg, details);
+    }
+
+
+    /**
+     * executes jobs until toDo is empty
+     * @emits Reporter:jobStarted
+     * @emits Reporter:jobFinished
+     * @emits Reporter:done
+     */
+    run(cb) {
+        console.log('cb', cb);
+        const _this = this;
+        while(this.runAnother) {
+            const job = this.toDo.shift();
+            _this.running[job.id] = job;
+            _this.report('jobStarted', job);
+            job
+                .exec()
+                .then(() => {
+                    _this.report('jobFinished', job);
+                    _this.complete.push(job);
+                    delete _this.running[job.id];
+                    _this.run();
+                });
+        }
+        if(_this.done) {
+            _this.report('done');
+            console.log(typeof cb);
+            return cb(null, _this);
+            // return Promise.resolve(_this);
+        }
+    }
+}
+
+
+exports.Job = Job;
+exports.JobQueue = JobQueue;
---
+++ jobs.spec.js
@@ -0,0 +1,195 @@
+/* eslint-disable no-magic-numbers */
+const chai = require('chai');
+const { expect } = chai;
+const sinon = require('sinon');
+chai.use(require('sinon-chai'));
+
+const { Job, JobQueue } = require('./jobs.js');
+
+describe('Jobs Module', () => {
+    describe('Job', () => {
+        it('should invoke the payload on exec', async () => {
+            const payloadFake = sinon.fake.resolves('hey');
+            const params = { hey: 1, you: 2 };
+            const job = new Job(payloadFake, params);
+            expect(job.isDone).to.be.false;
+
+            const result = await job.exec();
+
+            expect(result).to.equal('hey');
+            expect(job.isDone).to.be.true;
+            expect(payloadFake).calledOnceWith(params);
+        });
+        it('should trap non-promise payloads on exec', async () => {
+            const payloadFake = sinon.fake.returns('hey');
+            const params = { hey: 1, you: 2 };
+            const job = new Job(payloadFake, params);
+            expect(job.isDone).to.be.false;
+            expect(job.error).to.be.undefined;
+
+            const result = await job.exec();
+
+            expect(result).keys('error');
+            expect(job.error).not.to.be.empty;
+            expect(job.isDone).to.be.true;
+            expect(payloadFake).calledOnceWith(params);
+        });
+        it('should have status text', async () => {
+            const payloadFake = sinon.fake.resolves('hey');
+            const params = { hey: 1, you: 2 };
+            const job = new Job(payloadFake, params);
+            expect(job.isDone).to.be.false;
+            expect(job.status).equals('waiting');
+
+            const result = await job.exec();
+
+            expect(result).to.equal('hey');
+            expect(job.status).to.equal('done');
+            expect(job.error).to.be.undefined;
+            expect(job.isDone).to.be.true;
+            expect(payloadFake).calledOnceWith(params);
+        });
+        it('should have some properties', () => {
+            const payloadFake = sinon.fake.resolves('hey');
+            const job = new Job(payloadFake);
+            expect(job.name).to.be.undefined;
+
+            job.name = 'name';
+
+            expect(job.id).not.to.be.empty;
+            expect(job.name).equals('name');
+        });
+    });
+    describe('JobQueue', () => {
+        describe('constructor', () => {
+            it('should throw if a job is not instance of Job', () => {
+                const wrap = () => {
+                    new JobQueue([ 1 ]);
+                };
+                expect(wrap).to.throw(TypeError);
+            });
+        });
+        describe('runAnother', () => {
+            it('should be true ', () => {
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                jobs.push(new Job(payloadFake));
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+
+                expect(q.runAnother).to.be.true;
+            });
+            it('should be false', () => {
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                jobs.push(new Job(payloadFake));
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+                expect(q.runAnother).to.be.true;
+
+                q.running.fake = q.toDo.shift();
+
+                expect(q.runAnother).to.be.false;
+            });
+            it('should be false', () => {
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                jobs.push(new Job(payloadFake));
+                jobs.push(new Job(payloadFake));
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+                expect(q.runAnother).to.be.true;
+
+                q.running.fake = q.toDo.shift();
+
+                expect(q.runAnother).to.be.false;
+            });
+        });
+        describe('done', () => {
+            it('should be true', () => {
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                jobs.push(new Job(payloadFake));
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+                expect(q.done).to.be.false;
+
+                q.complete.push(q.toDo.shift());
+
+                expect(q.done).to.be.true;
+            });
+            it('should be false', () => {
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                jobs.push(new Job(payloadFake));
+                jobs.push(new Job(payloadFake));
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+                expect(q.done).to.be.false;
+
+                q.complete.push(q.toDo.shift());
+
+                expect(q.done).to.be.false;
+            });
+            it('should be false', () => {
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                jobs.push(new Job(payloadFake));
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+                expect(q.done).to.be.false;
+
+                q.running.fake = 'who cares';
+
+                q.complete.push(q.toDo.shift());
+
+                expect(q.done).to.be.false;
+            });
+        });
+        describe.only('run', () => {
+            it('should execute the job and emit status', async function(done) {
+                this.timeout(3000);
+                const payloadFake = sinon.fake.resolves('hey');
+                const jobs = [];
+                const maxConcurrentJobs = 1;
+                this.jobFinishedCount = 0;
+                this.jobStartedCount = 0;
+                this.doneCount = 0;
+                jobs.push(new Job(payloadFake));
+                jobs.push(new Job(payloadFake));
+                const tester = (error, { toDo, running, complete }) => {
+                    console.log('wtf');
+                    expect(error).to.be.undefined;
+                    expect(toDo).to.equal(0);
+                    expect(running).to.equal(0);
+                    expect(complete).to.equal(2, 'complete should be same as number of jobs');
+
+                    expect(this.jobFinishedCount).to.equal(2, 'jobFinishedCount should be 2');
+                    expect(this.jobStartedCount).to.equal(2, 'jobStartedCount should be 2');
+
+                    done();
+                };
+
+                const q = new JobQueue(jobs, maxConcurrentJobs);
+
+                q.reporter.on('jobStarted', ({ running }) => {
+                    this.jobStartedCount++;
+                    expect(running).is.lessThan(maxConcurrentJobs + 1, 'should be less than max concurrent jobs');
+                });
+
+                q.reporter.on('jobFinished', () => {
+                    this.jobFinishedCount++;
+                });
+
+                q.run(tester);
+
+            });
+        });
+    });
+});
